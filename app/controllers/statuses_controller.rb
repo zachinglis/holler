c9f76dd832537e9148dab0ce941f0d29c0038768
Zach Inglis <zach@lt3media.com>
2008-06-22 15:41:50 -0400

    Styled up page

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -2,7 +2,7 @@ class StatusesController < ApplicationController
   before_filter :login_required
   
   def index
-    @statuses = Status.since(1.week.ago).find(:all, :order => "created_at DESC").group_by { |status| status.created_at.pretty_date }
+    @statuses = Status.since(1.week.ago).find(:all, :order => "created_at DESC").group_by { |status| status.created_at.strftime("%j") }
     
     respond_to do |wants|
       wants.html
