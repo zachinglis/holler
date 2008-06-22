d89fc6cf9c44b40a3a0ece7f00bb8780d43a8948
Zach Inglis <zach@lt3media.com>
2008-06-22 00:56:50 -0400

    adding status controller

---
+++ app/controllers/statuses_controller.rb
@@ -0,0 +1,2 @@
+class StatusesController < ApplicationController
+end
---
+++ spec/controllers/statuses_controller_spec.rb
@@ -0,0 +1,10 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe StatusesController do
+
+  #Delete this example and add some real ones
+  it "should use StatusesController" do
+    controller.should be_an_instance_of(StatusesController)
+  end
+
+end
