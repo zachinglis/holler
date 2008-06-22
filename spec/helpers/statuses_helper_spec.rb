a45211cd4ecbf8d916ec1e6d69c0aeed55543e86
Zach Inglis <zach@lt3media.com>
2008-06-22 16:03:10 -0400

    Status helper spec now has full coverage

--- spec/helpers/statuses_helper_spec.rb
+++ spec/helpers/statuses_helper_spec.rb
@@ -1,11 +1,20 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe StatusesHelper do
+  include StatusesHelper
   
-  #Delete this example and add some real ones or delete this file
-  it "should be included in the object returned by #helper" do
-    included_modules = (class << helper; self; end).send :included_modules
-    included_modules.should include(StatusesHelper)
+  describe ".today_tomorow_or_day" do
+    it "should return the correct value for todays date" do
+      today_tomorow_or_day(Time.today).should eql("Today")
+    end
+    
+    it "should return the correct value for tomorows date" do
+      today_tomorow_or_day(1.day.ago).should eql("Yesterday")
+    end
+    
+    it "should return the correct value for any other date" do
+      today_tomorow_or_day(4.days.ago).should eql(4.days.ago.strftime("%A"))
+    end
   end
 
 end
