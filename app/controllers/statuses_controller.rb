50ad97654130f3409c7e790ebe78fbc85e36dc61
Zach Inglis <zach@lt3media.com>
2008-06-22 14:31:00 -0400

    Cleaned up the newer statuses call

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -2,7 +2,7 @@ class StatusesController < ApplicationController
   before_filter :login_required
   
   def index
-    @statuses = Status.all.group_by { |status| status.created_at.pretty_date }
+    @statuses = Status.find(:all, :order => "created_at DESC").group_by { |status| status.created_at.pretty_date }
     # .group_by(&:user)
     
     respond_to do |wants|
