738f958ab05cdf5616603853eb525a281345b66d
Zach Inglis <zach@lt3media.com>
2008-06-22 21:21:21 -0400

    Fixed a time bug in the specs

--- spec/helpers/statuses_helper_spec.rb
+++ spec/helpers/statuses_helper_spec.rb
@@ -3,21 +3,21 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe StatusesHelper do
   include StatusesHelper
   
-  describe ".today_tomorow_or_day" do
+  describe "#today_tomorow_or_day" do
     it "should return the correct value for todays date" do
-      today_tomorow_or_day(Time.today).should eql("Today")
+      today_tomorow_or_day(Time.now.utc).should eql("Today")
     end
     
-    it "should return the correct value for tomorows date" do
-      today_tomorow_or_day(1.day.ago).should eql("Yesterday")
+    it "should return the correct value for yesterdays date" do
+      today_tomorow_or_day(1.day.ago.utc).should eql("Yesterday")
     end
     
     it "should return the correct value for any other date" do
-      today_tomorow_or_day(4.days.ago).should eql(4.days.ago.strftime("%A"))
+      today_tomorow_or_day(4.days.ago.utc).should eql(4.days.ago.strftime("%A"))
     end
   end
 
-  describe ".user_class" do
+  describe "#user_class" do
     it "should return 'higlight' if user is the current_user" do
       user, current_user = users(:quentin), users(:quentin)
       user_class(user, current_user).should eql('highlight')
